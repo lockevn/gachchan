485ef867cbc20593f9f245c41851b1db12f3240b
Thach Lockevn <lockevn@gmail.com>
2021-07-28 18:26:36 +0700

    test export default CommontHelper

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-"use strict";
+// "use strict";
 
 var Util                        = require("./src/lib/Util");
 var AuthHelper                  = require("./src/lib/AuthHelper");
 var ReactiveSymbolHistoryStore  = require("./src/lib/ReactiveSymbolHistoryStore");
 var XAPIService                 = require("./src/services/XAPIService");
 
+import CommonHelper from "./src/CommonHelper"
 
 // Exports
 module.exports.Util = Util;
 module.exports.AuthHelper = AuthHelper;
 module.exports.ReactiveSymbolHistoryStore = ReactiveSymbolHistoryStore;
 module.exports.XAPIService = XAPIService;
+
+export default  {
+    CommonHelper
+}
---
+++ src/CommonHelper.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+
+class CommonHelper {   
+  static hello(path) {
+    return `Hello ${path}` 
+  }
+}
+
+export default CommonHelper;
