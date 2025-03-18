2d734253db83e1aaef38ed21e1b954423ad5e5aa
Thach Lockevn <lockevn@gmail.com>
2025-03-18 20:38:05 +0700

    refactor: remove StockHelper, rename helper to helpers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { CommonHelper } from './helper/CommonHelper'
-import { UtilHelper } from './helper/UtilHelper'
-import { HtmlHelper } from './helper/HtmlHelper'
-import { DateTimeHelper } from './helper/DateTimeHelper'
-import { StockvnHelper } from './helper/StockvnHelper'
-import { AuthHelper } from './helper/AuthHelper'
-import { TextHelper } from './helper/TextHelper'
+import { CommonHelper } from './helpers/CommonHelper'
+import { UtilHelper } from './helpers/UtilHelper'
+import { HtmlHelper } from './helpers/HtmlHelper'
+import { DateTimeHelper } from './helpers/DateTimeHelper'
+import { StockvnHelper } from './helpers/StockvnHelper'
+import { AuthHelper } from './helpers/AuthHelper'
+import { TextHelper } from './helpers/TextHelper'
 // import { ReactiveSymbolHistoryStore } from "./helper/ReactiveSymbolHistoryStore.ts.bak"
 
 export {
